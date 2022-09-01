04eadd00499034a49fcdab2826566313c1ecbb9c
luisvillegasbf23 <luisville.lv@gmail.com>
2022-08-31 21:11:46 -0300

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert node left in the binary tree
+ * @parent: parent node
+ * @value: data node
+ * Return: pointer node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	binary_tree_node(parent, value);
+
+	if (parent->left == NULL)
+	{
+		parent->left = new;
+		new->parent = parent;
+		return (new);
+	}
+	else
+	{
+		new->left = parent->left;
+		new->parent = parent;
+		parent->left = new;
+		parent->left->left->parent = new;
+		return (new);
+	}
+}
